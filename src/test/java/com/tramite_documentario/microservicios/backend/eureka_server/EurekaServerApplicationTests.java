77f17f5dd6fb165e3ae8992e4c7ce7a5a797f08d
Gino Ascencio <gino.francisco@hotmail.com>
2020-12-24 18:08:46 -0500

    Primer Commit - Eureka Server

---
+++ src/test/java/com/tramite_documentario/microservicios/backend/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tramite_documentario.microservicios.backend.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
